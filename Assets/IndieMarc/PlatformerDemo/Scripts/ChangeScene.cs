66b30acf93dc04ff088c62f16f45363a3bfc852c
Aydar <me@aydar.media>
2024-10-20 02:28:33 +0300

    farm bruh

--- Assets/IndieMarc/PlatformerDemo/Scripts/ChangeScene.cs
+++ Assets/IndieMarc/PlatformerDemo/Scripts/ChangeScene.cs
@@ -4,32 +4,41 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace IndieMarc.Platformer {
-    public class BoundaryCheck : MonoBehaviour {
+    public class ChangeScene : MonoBehaviour {
         public string targetScene;
 
         public Image fadeImage;
 
-        IEnumerator FadeOut(float duration) {
+        private float defaultSpeed = 1f;
+
+        IEnumerator Fade(bool isOut, float duration) {
             Color color = fadeImage.color;
-            color.a = 0;
+            float startAlpha = isOut ? 1 : 0;
+            float endAlpha = isOut ? 0 : 1;
             float time = 0;
 
-            while (time < duration) {
-                fadeImage.color = new Color(color.r, color.g, color.b, time / duration);
-                time += Time.deltaTime;
-                yield return null;
+            while (time < duration)
+            {
+            float alpha = Mathf.Lerp(startAlpha, endAlpha, time / duration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            time += Time.deltaTime;
+            yield return null;
             }
 
-            fadeImage.color = new Color(color.r, color.g, color.b, 1);
+            fadeImage.color = new Color(color.r, color.g, color.b, endAlpha);
         }
 
         IEnumerator SwitchScene(string sceneName, float duration) {
-            yield return StartCoroutine(FadeOut(duration));
+            yield return StartCoroutine(Fade(false, duration));
             SceneManager.LoadScene(sceneName);
         }
 
+        void Awake() {
+            StartCoroutine(Fade(true, defaultSpeed));
+        }
+
         void OnTriggerEnter2D(Collider2D other) {
-            if (other.gameObject.tag == "Player") StartCoroutine(SwitchScene(targetScene, 2f));
+            if (other.gameObject.tag == "Player") StartCoroutine(SwitchScene(targetScene, defaultSpeed));
         }
     }
 }
